da36cee23f0b5c4a4edea3e442b2c018e608f352
gsmaloto <genermaloto@gmail.com>
2025-02-02 20:51:30 +0800

    Add env file

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -1,11 +1,31 @@
 import Navbar from "@/components/shared/navbar";
-import { movieDetails } from "@/lib/data/movie-details";
 import VideoPlayer from "@/components/shared/video-player";
 import MovieDetails from "./components/movie-details";
 import BgImageOverlay from "@/components/shared/bg-image-overlay";
 
-export default function WatchPage({ params }: { params: { id: string } }) {
+const fetchMovieDetails = async (id: string) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+    },
+    cache: "no-store",
+  };
+
+  const res = await fetch(url, options);
+  if (!res.ok) throw new Error("Failed to fetch movie details");
+  return res.json();
+};
+
+export default async function WatchPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = params;
+  const movieDetails = await fetchMovieDetails(id);
 
   return (
     <main className="relative min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
