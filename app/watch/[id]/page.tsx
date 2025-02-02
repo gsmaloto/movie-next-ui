ad4d83602b5c16540c43b9ea4534ac84dc3843b5
gsmaloto <genermaloto@gmail.com>
2025-02-02 20:41:50 +0800

    Remove comment code

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -7,67 +7,6 @@ import BgImageOverlay from "@/components/shared/bg-image-overlay";
 export default function WatchPage({ params }: { params: { id: string } }) {
   const { id } = params;
 
-  // const posterPath = movieDetails?.poster_path
-  //   ? `https://image.tmdb.org/t/p/w500${movieDetails?.poster_path}`
-  //   : "https://www.popcorn.app/assets/app/images/placeholder-movieimage.png";
-
-  // const rating = movieDetails?.vote_average || 0;
-  // const maxRating = 10;
-
-  // const trailerKey = movieVideos?.results?.find(
-  //   (video: any) => video.type === "Trailer"
-  // )?.key;
-
-  // const trailerUrl = `https://www.youtube.com/embed/${trailerKey}`;
-  // console.log(trailerUrl);
-
-  // const movieData = [
-  //   {
-  //     label: "Runtime",
-  //     value: `${movieDetails?.runtime} min`,
-  //   },
-  //   {
-  //     label: "Release Date",
-  //     value: movieDetails?.release_date,
-  //   },
-  //   {
-  //     label: "Popularity",
-  //     value: movieDetails?.popularity,
-  //   },
-  //   {
-  //     label: "Language",
-  //     value: movieDetails?.original_language,
-  //   },
-  //   {
-  //     label: "Tagline",
-  //     value: movieDetails?.tagline,
-  //   },
-  //   {
-  //     label: "Status",
-  //     value: movieDetails?.status,
-  //   },
-  //   {
-  //     label: "Budget",
-  //     value: movieDetails?.budget?.toLocaleString(),
-  //   },
-  //   {
-  //     label: "Revenue",
-  //     value: movieDetails?.revenue?.toLocaleString(),
-  //   },
-  //   {
-  //     label: "Production Countries",
-  //     value: movieDetails?.production_countries
-  //       .map((country) => country.name)
-  //       .join(", "),
-  //   },
-  //   {
-  //     label: "Spoken Languages",
-  //     value: movieDetails?.spoken_languages
-  //       .map((lang) => lang.english_name)
-  //       .join(", "),
-  //   },
-  // ];
-
   return (
     <main className="relative min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
       <Navbar />
